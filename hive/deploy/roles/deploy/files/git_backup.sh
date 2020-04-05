ea3180697ac012fca78e803eb7f0efe8a4aca756
Berveglieri <felipedimitri5@gmail.com>
2020-04-05 17:10:10 +0200

    added git push backups script

--- hive/deploy/roles/deploy/files/git_backup.sh
+++ hive/deploy/roles/deploy/files/git_backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 eval "$(ssh-agent -s)"
-ssh-add "$HOME/.ssh/my-key"
+ssh-add "$HOME/.ssh/pkey"
 
 TEMP="/tmp"
 REPO="git@github.com:Berveglieri/challenge.git"
@@ -11,8 +11,8 @@ cd $TEMP
 git clone $REPO
 cd $STRUCT
 
-sudo pgtool -operation backup -h app-db.c1vfijgzuyxj.eu-central-1.rds.amazonaws.com -p 5432 -u master -P 'pDgdCQatKGuWKKjxycqwvMoZMUeaXwnwWG' -d hive
+sudo pgtool -operation backup -h app-db.c1vfijgzuyxj.eu-central-1.rds.amazonaws.com -p 5432 -u master -P '' -d hive
 
 git add . && git commit -m "new backup" && git push
 
-cd $TEMP $$ rm -rf challenge
+cd $TEMP && rm -rf challenge
